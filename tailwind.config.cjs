cebf78e039cd5a9b020174764aea3d907dbe0ec4
Santiagorodriguezgalviz <rodriguezsanti95@gmail.com>
2024-11-24 11:34:12 -0500

    landing-page-Wilson

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4285F4',
+        'primary-dark': '#3367D6',
+      },
+    },
+  },
+  plugins: [],
+}
